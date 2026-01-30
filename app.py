b6ad848cdba0b498ef19df119042b24c58824237
TiagoGazeta <gazetamonteiro@gmail.com>
2026-01-30 20:33:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -208,7 +208,7 @@ INSTRUCOES_MESTRE = f"""
 
 # 1. Configuração do Modelo (Ajustado para o nome oficial)
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
-MODELO_GROQ = "llama-3.3-70b-versatile"
+MODELO_GROQ = "meta-llama/llama-4-scout-17b-16e-instruct"
 
 # --- CONFIGURAÇÃO DA API ---
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
